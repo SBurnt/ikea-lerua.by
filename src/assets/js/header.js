984196398d2447237fa80fd2cbabcd487f3062d4
SBurnt <zip2004zip@gmail.com>
2021-02-26 12:40:39 +0300

    added mobile header and menu

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -29,3 +29,36 @@ window.addEventListener('resize', function() {
 	headerScrollDescRes();
 });
 // header scroll on desktop END
+
+// header mob menu open START
+const mobMenu = document.querySelector('.header__inner');
+const burger = document.querySelector('.js-burger');
+const btnCloseMenu = document.querySelector('.js-close-mob-menu');
+const overlay = document.querySelector('.overlay');
+
+// const lockScroll = () => {
+// 	document.body.classList.add('lock');
+// };
+
+// const unlockScroll = () => {
+// 	document.body.classList.remove('lock');
+// };
+
+burger.addEventListener('click', () => {
+	mobMenu.classList.add('open');
+	overlay.classList.add('open');
+	// lockScroll();
+});
+
+overlay.addEventListener('click', () => {
+	mobMenu.classList.remove('open');
+	overlay.classList.remove('open');
+	// unlockScroll();
+});
+
+btnCloseMenu.addEventListener('click', () => {
+	mobMenu.classList.remove('open');
+	overlay.classList.remove('open');
+	// unlockScroll();
+});
+// header mob menu open END
