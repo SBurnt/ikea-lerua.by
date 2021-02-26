413b7b84ec92789b7b152c3d8112ac39f3786483
SBurnt <zip2004zip@gmail.com>
2021-02-26 16:45:31 +0300

    added responsive to the main page

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -6,6 +6,7 @@ if (sliderList) {
 	sliderList.forEach(el => {
 		const swiper = new Swiper(el, {
 			loop: true,
+			autoHeight: true,
 			// autoplay: {
 			// 	delay: 5000,
 			// 	disableOnInteraction: false,
@@ -13,6 +14,12 @@ if (sliderList) {
 			navigation: {
 				nextEl: '.slider__arrows-next',
 				prevEl: '.slider__arrows-prev',
+			},
+			breakpoints: {
+				// when window width is >= 768px
+				768: {
+					autoHeight: false,
+				},
 			},
 		});
 	});
